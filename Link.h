673cb9d1ca7de5f0eefc89fc4cabe5938b70e093
Vivian He <vivianhe@Vivians-MacBook-Pro.local>
2015-10-22 20:56:23 -0700

    Updated link class

--- Link.h
+++ Link.h
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <queue>
+#include "naivepacket.h"
 
 using namespace std;
 
 class Link {
+	// Maximum rate in Mbps that the link may transmit
 	int capacity;
+	// Total flow rate through the link
 	int flowrate;
+	// Indicates the direction of the transmission
+	bool direction;
+	// Time in ms required to transmit packet
 	int delay;
+	// Number of packets that the link can store in queue
 	int buffersize;
+	// Queue of packets to be transmitted
 	std::queue <Naive_Packet> buffer;
 
 public:
@@ -19,5 +27,8 @@ public:
 
 	// Add a packet to the link's buffer.
 	void add_to_buffer(Naive_Packet packet);
-	
+
+	// Return a link cost dependent on transmission delay, flow rate, and
+	// flow direction.
+	int calculate_cost(int delay, int flowrate, bool direction);
 };
--- link.cpp
+++ link.cpp
@@ -8,7 +8,7 @@ Link::Link() {
 	// Initialize queue in here?
 }
 
-void Link::set_values(int cap, in flow, int delay, int buf) {
+void Link::set_values(int cap, int flow, int delay, int buf) {
 	capacity = cap;
 	flowrate = flow;
 	delay = delay;
@@ -18,7 +18,12 @@ void Link::set_values(int cap, in flow, int delay, int buf) {
 void Link::add_to_buffer(Naive_Packet packet) {
 	// If the buffer is not full, enqueue the incoming packet.
 	if (buffer.size() < buffersize) {
-		buffer.push(Naive_Packet)
+		buffer.push(packet);
 	}
 	// Else, the buffer is full. The packet is dropped.
+}
+
+int Link::calculate_cost(int delay, int flowrate, bool direction) {
+	int cost = delay + flowrate;
+	return cost;
 }
