d09219698114794b121d5ba3a0139d5d23737e0b
jordanbonilla <jbonilla@caltech.edu>
2015-11-29 18:19:13 -0800

    fixed bug

--- source/runproj.m
+++ source/runproj.m
@@ -1,6 +1,4 @@
+clear mex;
 %Note that print statements are not guarantted to be in order.
-
-clear mex;
 %Testcase file, max time to run (s), TCP ID
-proj('testcase1.txt', 100.0, 1);
-clear mex;
+[x,y] = projmex('testcase1.txt', 100.0, 1);
